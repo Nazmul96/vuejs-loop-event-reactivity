bf661cbc25955d97a12b7477cc9e3c9bd95fa4a1
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-21 21:58:16 +0600

    practice loop with indexing

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+ ];
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   <div class="p-5 border border-gray-600 w-50" v-for="(task,index) in tasks">{{ index+1}}.{{ task }}</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
