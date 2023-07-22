c38e3e8c0cb7638e8fae7039cd97d1fdffac5d30
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-22 14:27:39 +0600

    click event practice

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
 <script setup>
-    const persons = [
-    {
-      id: 1,
-      name: "Jane Doe",
-      profilePhoto: "//tailus.io/sources/blocks/classic/preview/images/woman1.jpg",
-      jobTitle: "CEO, Co-Founder",
-    },
-    {
-      id: 2,
-      name: "John Doe",
-      profilePhoto: "//tailus.io/sources/blocks/classic/preview/images/man.jpg",
-      jobTitle: "CTO",
-    },
-    {
-      id: 3,
-      name: "Jessy Doe",
-      profilePhoto: "//tailus.io/sources/blocks/classic/preview/images/woman.jpg",
-      jobTitle: "COO",
-    }
-  ];
+  import {ref} from 'vue'
+  const location = ref("Dhaka")
+
+  function changeLocation(newLocation){
+    location.value = newLocation;
+  }
 </script>
 
 <template>
-    <section class="mx-auto container">
-    <div class="py-20 bg-gray-50">
-      <div class="container mx-auto px-6 px-32">
-        <div class="mb-16 text-center">
-          <h2 class="mb-4 text-center text-2xl text-gray-900 font-bold text-4xl">Team Member</h2>
-          <p class="text-gray-600 w-8/12 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni deleniti veniam laudantium itaque perferendis dolor maxime incidunt voluptatem. </p>
+    <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
+        <div class="container mx-auto flex space-x-5 justify-center m-5">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
+              Dhaka
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rajshahi')">
+                Rajshahi
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna')">
+                Khulna
+            </button>
         </div>
-        <div class="grid gap-12 items-center grid-cols-3">
-          
-          <div class="space-y-4 text-center" v-for="person in persons" :key="person.id">
-            <img class="mx-auto object-cover rounded-xl w-64" :class="2==person.id ? 'h-80':'h-60'" :src="person.profilePhoto">
-            <!-- <img class="mx-auto object-cover rounded-xl w-64" :class="1==index?'h-80':'h-64'" :src="person.profilePhoto"> -->
-            <div>
-              <h4 class="text-2xl">{{ person.name }}</h4>
-              <span class="block text-sm text-gray-500">{{ person.jobTitle }}</span>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-    </div>
-  </section>
+        <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
+    </section>
 </template>
 
 <style scoped>
