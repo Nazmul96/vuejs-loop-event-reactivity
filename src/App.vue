d4267f310759a5438bb4b090e4ac85c045f341c9
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-26 00:23:06 +0600

    module 4 live test done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 
-  const items = [1,2,3,4,5]
+   import ReactiveObjectComponent from './components/ReactiveObjectComponent.vue'
 
 </script>
 
 <template>
-    <div class="p-5 border border-gray-600 w-40" v-for="item in items">{{ item }}</div>
+   <ReactiveObjectComponent />
 </template>
 
 <style scoped>
---
+++ src/components/ReactiveObjectComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+   import { reactive } from 'vue'; 
+   const  data = reactive(
+    {
+        count: 0,
+    }
+   );
+
+   function incrementValue(){
+      data.count++; 
+   }
+</script>
+
+<template>
+        <div class="">
+            <p>Count: {{ data.count }}</p><br>
+            <button class="btn btn-primary text-black" type="button" @click="incrementValue()">Increment</button>
+         </div>
+</template>
+
+<style scoped>
+
+</style>
