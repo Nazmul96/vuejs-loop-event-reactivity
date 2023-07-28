98e1358b3b94e45fecd4013aad00b31a4f2a0bf2
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-28 21:29:35 +0600

    country name show with specific starting character

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import {countries} from './data/country';
+  const name_start = "B";
+  const country_find = countries.filter(
+    (country) => country.country.toUpperCase().startsWith(name_start)
+  )
+  
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+      <h1>Country Name Start With {{ name_start }}</h1>
+      <div>
+        <p v-for="(country,index) in country_find" :key="index" style="width: 50px;text-align:center;">{{index+1}}:{{ country.country }}</p>
+      </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
